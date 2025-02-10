b49273f93d287676d3e44a0427bd3169f900827f
bsjhx <rakoczymichal@hotmail.com>
2025-02-10 20:38:15 +0100

    Initial commit

---
+++ src/main/java/com/bsjhx/cash_flow_es/CashFlowEsApplication.java
@@ -0,0 +1,13 @@
+package com.bsjhx.cash_flow_es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsjhx/cash_flow_es/CashFlowEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsjhx.cash_flow_es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
